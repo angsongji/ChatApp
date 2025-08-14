65c28ebfad66195276708b2a0e0de87484840ce3
angsongji <hoichima33@gmail.com>
2025-08-14 23:28:47 +0700

    update ip

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://3.81.230.117",
     credentials: true,
   })
 );
--- frontend/src/services/axiosClient.js
+++ frontend/src/services/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "http://3.81.230.117:5001/api",
   withCredentials: true,
   timeout: 20000, // 20s timeout
 });
